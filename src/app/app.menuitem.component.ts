f844ef9aec581e3215238b068243ba71ed08a9e0
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2020-04-01 13:05:51 +0000

    lint fix

--- src/app/app.menuitem.component.ts
+++ src/app/app.menuitem.component.ts
@@ -29,7 +29,8 @@ import {AppComponent} from './app.component';
                <span class="menuitem-badge" *ngIf="item.badge">{{item.badge}}</span>
 			</a>
 			<ul *ngIf="item.items && active"
-				[@children]="((app.isHorizontal() || app.isSlim()) && root) ? (active ? 'visible' : 'hidden') : (active ? 'visibleAnimated' : 'hiddenAnimated')">
+				[@children]="((app.isHorizontal() || app.isSlim()) && root) ? (active ? 'visible' : 'hidden') :
+				(active ? 'visibleAnimated' : 'hiddenAnimated')">
 				<ng-template ngFor let-child let-i="index" [ngForOf]="item.items">
 					<li app-menuitem [item]="child" [index]="i" [parentKey]="key" [class]="child.badgeClass"></li>
 				</ng-template>
