e2957c80d5e7ab36d8100bd65620982c2ccc83d6
yigitfindikli <yigitfindikli@fd9223c1-c77d-41e1-af93-17165896985c>
2020-04-01 02:42:17 +0000

    Test fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,31 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { AppMenuComponent, AppSubMenuComponent } from './app.menu.component';
+import { AppMenuComponent } from './app.menu.component';
 import { AppTopBarComponent } from './app.topbar.component';
 import { AppFooterComponent } from './app.footer.component';
 import { AppRightPanelComponent } from './app.rightpanel.component';
 import { AppBreadcrumbComponent } from './app.breadcrumb.component';
 import { BreadcrumbService } from './breadcrumb.service';
-import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { TabViewModule } from 'primeng/tabview';
-import { FormsModule } from '@angular/forms';
+import { MenuService } from './app.menu.service';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
                 RouterTestingModule,
-                ScrollPanelModule,
-                ScrollPanelModule,
                 TabViewModule
             ],
             declarations: [
                 AppComponent,
                 AppMenuComponent,
-                AppSubMenuComponent,
                 AppRightPanelComponent,
                 AppTopBarComponent,
                 AppFooterComponent,
                 AppBreadcrumbComponent
             ],
-            providers: [BreadcrumbService]
+            providers: [BreadcrumbService, MenuService]
         }).compileComponents();
     }));
 
