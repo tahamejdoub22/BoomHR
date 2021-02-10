a396233885cdf3f7a77d1e20b8ce9e00f80fb1b0
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-02-10 12:33:05 +0300

    table demo updated

--- src/app/demo/view/tabledemo.component.ts
+++ src/app/demo/view/tabledemo.component.ts
@@ -8,7 +8,15 @@ import {BreadcrumbService} from '../../app.breadcrumb.service';
 
 @Component({
     templateUrl: './tabledemo.component.html',
-    styleUrls: ['./tabledemo.scss']
+    styleUrls: ['./tabledemo.scss'],
+    styles: [`
+        @media screen and (max-width: 960px) {
+            :host ::ng-deep .p-datatable.p-datatable-customers.rowexpand-table .p-datatable-tbody > tr > td:nth-child(6) {
+                display: flex;
+            }
+        }
+
+    `],
 })
 export class TableDemoComponent implements OnInit {
 
@@ -30,6 +38,8 @@ export class TableDemoComponent implements OnInit {
 
     rowGroupMetadata: any;
 
+    activityValues: number[] = [0, 100];
+
     @ViewChild('dt') table: Table;
 
     constructor(private customerService: CustomerService, private productService: ProductService,
@@ -40,7 +50,11 @@ export class TableDemoComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.customerService.getCustomersMedium().then(customers => this.customers1 = customers);
+        this.customerService.getCustomersLarge().then(customers => {
+            this.customers1 = customers;
+            // @ts-ignore
+            this.customers1.forEach(customer => customer.date = new Date(customer.date));
+        });
         this.customerService.getCustomersMedium().then(customers => this.customers2 = customers);
         this.customerService.getCustomersMedium().then(customers => this.customers3 = customers);
         this.productService.getProductsWithOrdersSmall().then(data => this.products = data);
